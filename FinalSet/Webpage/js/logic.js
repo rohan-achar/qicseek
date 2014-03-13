bf76ee51cdc7420dace98bdf234877e92c067846
Sivabalan Thirunavukkarasu <siva886@gmail.com>
2014-03-12 19:31:46 -0700

    Auto-suggest implemented

--- FinalSet/Webpage/js/logic.js
+++ FinalSet/Webpage/js/logic.js
@@ -1,25 +1,120 @@
+var gVar;
+
 function init() {
-	$('#searchButton').click(function(i,obj){
-		sendSearchRequest();
+
+	var searchBox = document.getElementById('searchText'),
+		$sBoxObj = $(searchBox),
+		$asObj = $('#autoSuggestDiv');
+
+	searchBox.addEventListener('input',function(e){
+		console.log(e);
+		gVar = e;
+		var searchQuery = e.target.value,
+			asQueryObj = getWordAtPosition(searchQuery,e.target.selectionStart);
+
+		if(e.target.value.slice(-1) == ' ' || e.target.value.replace(' ','') == '' || $asObj.text().slice(1,-1) == asQueryObj.word)
+		{	
+			$asObj.hide();
+		}
+		else
+		{
+			if(asQueryObj.word.length >= 3) {
+				autoSuggest(e.target,asQueryObj);
+			}
+		}
 	});
+
+	$asObj.css('top',($sBoxObj.offset().top + $sBoxObj.outerHeight()).toString()+'px');
+
 	$(document).keypress(function(e) {
 		if($('#searchText').is(':focus'))
 		{
-	    	if(e.which == 13) {
+			var keynum;
+
+			if(window.event) 
+			{ // IE					
+            	keynum = e.keyCode;
+            }
+            else if(e.which)
+            { // Netscape/Firefox/Opera					
+            	keynum = e.which;
+            }
+            
+	    	if(keynum == 13) 
+	    	{ // Enter key is pressed
 	        	sendSearchRequest();
-	    	}
-	    }
+	        	return;
+	    	}    
+        }
+	    
+	});
+
+	$('#searchButton').click(function(i,obj){
+		sendSearchRequest();
 	});
 }
 
+function getWordAtPosition(inputString, cPosition) {
+	var i = cPosition,
+		j = cPosition,
+		wordObj = {};
 
-function inputValidation() {
+	while(i > 0 && inputString[i-1] != ' ') {
+		i--;
+	}
 
+	while(j < inputString.length && inputString[j] != ' ') {
+		j++;
+	}
+
+	wordObj['word'] = inputString.slice(i,j);
+	wordObj['start'] = i;
+	wordObj['end'] = j;
+
+	return wordObj;
 }
 
-function autoSuggest() {
+function autoSuggest(inputObj,asWordObj) {
+	var $sBoxObj = $(inputObj),
+		$asObj = $('#autoSuggestDiv'),
+		asFixedLeft = $sBoxObj.offset().left + parseInt($sBoxObj.css('padding-left').replace('px','')),
+		asFixedOffset = parseInt($sBoxObj.css('padding-left').replace('px','')),
+		asOffset = $('#asSizeCheck')
+						.css('display','none')
+						.css('color','#fff')
+						.css('position','absolute')
+						.css('font-size',$sBoxObj.css('font-size'))
+						.css('width','auto')
+						.html($sBoxObj.val().slice(0,asWordObj.start))
+						.width();
 
+		asOffset = asWordObj.start > 0 ? asOffset : asOffset - 2; 
+		console.log(asOffset);
+
+		//$asObj.css('left',(asFixedLeft).toString()+'px');
+
+		$asObj.css('left',(asFixedLeft+asOffset+1).toString()+'px');
+
+	$.ajax({
+		type: "GET",
+		url: URL,
+		data: {'q': asWordObj.word, 'as': 'true'},
+		success: function(data) {
+			
+			searchResults = data;
+			
+			console.log(data);
+			if(data[0] != asWordObj.word)
+			{
+				$asObj.html('&nbsp;'+asWordObj.word+'<b>'+data[0].replace(asWordObj.word,'')+'</b>'+'&nbsp;')
+				$asObj.show();
+			}
+			
+		}
+	})
 }
+
+
 
 function sendSearchRequest() {
 	var queryText = $("#searchText").val();
